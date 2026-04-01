81a9203467f47faa3c00c84f83a6f5cd4bc6ffd3
johann2007 <Johann.bezerra27@gmail.com>
2026-03-31 21:39:30 -0300

    feat: implementa métodos de leitura, edição e exclusão no AlunoController e AlunoService

--- src/main/java/br/com/alunoonline/api/controller/AlunoController.java
+++ src/main/java/br/com/alunoonline/api/controller/AlunoController.java
@@ -6,8 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
-@RequestMapping
+@RequestMapping("/alunos")
 public class AlunoController {
     @Autowired
     AlunoService alunoService;
@@ -17,4 +20,29 @@ public class AlunoController {
     public void criarAluno(@RequestBody Aluno aluno){
         alunoService.criarAluno(aluno);
     }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Aluno> listarTodosAlunos() {
+        return alunoService.listarTodosAlunos();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Optional<Aluno> buscarAlunoPorId(@PathVariable Long id){
+        return alunoService.buscarAlunoPorId(id);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletarAlunoPorId(@PathVariable Long id){
+        alunoService.deletarAlunoPorId(id);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void atualizarAlunoPorId(@PathVariable Long id, @RequestBody Aluno alunoEditado){
+        alunoService.atualizarAluno(id, alunoEditado);
+    }
+
 }
--- src/main/java/br/com/alunoonline/api/service/AlunoService.java
+++ src/main/java/br/com/alunoonline/api/service/AlunoService.java
@@ -5,6 +5,9 @@ import br.com.alunoonline.api.repository.AlunoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class AlunoService {
     @Autowired
@@ -13,4 +16,23 @@ public class AlunoService {
     public void criarAluno(Aluno aluno){
         alunoRepository.save(aluno);
     }
+    
+    public List<Aluno> listarTodosAlunos() {
+
+        return alunoRepository.findAll();
+    }
+
+    public Optional<Aluno> buscarAlunoPorId(Long id){
+        return  alunoRepository.findById(id);
+    }
+
+    public void deletarAlunoPorId(Long id){
+        alunoRepository.deleteById(id);
+    }
+
+    public void atualizarAluno(Long id, Aluno alunoEditado){
+        alunoEditado.setId(id);
+        alunoRepository.save(alunoEditado);
+    }
+
 }
